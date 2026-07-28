d285ebc6aa820e61a1fc845fee518b7f652f64f9
Anjan <anjankumar102938@gmail.com>
2026-07-29 00:02:22 +0530

    fix7

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DART_VM_OPTIONS="--max-old-space-size=2560"
+export DART_VM_OPTIONS="--old_gen_heap_size=2048"
 cd app
 ../flutter/bin/flutter clean
 ../flutter/bin/flutter build web --release --dart-define=SUPABASE_URL="$SUPABASE_URL" --dart-define=SUPABASE_ANON_KEY="$SUPABASE_ANON_KEY" --dart-define=RAZORPAY_KEY_ID="$RAZORPAY_KEY_ID"
