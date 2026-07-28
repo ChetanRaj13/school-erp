91d88bef96113125698b4ba5e7d13488434a559e
Anjan <anjankumar102938@gmail.com>
2026-07-29 00:35:33 +0530

    fix11 - add dart vm limits back for vercel hobby OOM

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export DART_VM_OPTIONS="--old_gen_heap_size=1024"
+export NODE_OPTIONS="--max-old-space-size=2048"
 cd app
 ../flutter/bin/flutter clean
 ../flutter/bin/flutter build web --release --dart-define=SUPABASE_URL="$SUPABASE_URL" --dart-define=SUPABASE_ANON_KEY="$SUPABASE_ANON_KEY" --dart-define=RAZORPAY_KEY_ID="$RAZORPAY_KEY_ID"
